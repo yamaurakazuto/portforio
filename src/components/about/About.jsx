4c6705dda98d6c462f8db2d25c5dd3fc3bd2ba85
kazuto <xyst510@icloud.com>
2024-02-26 00:21:47 +0900

    add responsive

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,17 +7,16 @@ export default function About() {
       <div className="about-title">
         <h1>About</h1>
       </div>
-      <div className="imgcontainer">
+      <div className="img-container">
       <img src={foto} alt="" />
       </div>
       <div className="about-text">
-        <h2 className='title'>山浦麗斗</h2>
+        <h2>山浦麗斗</h2>
         <p>生年月日:1999年5月10</p>
         <p>趣味:テニス、筋トレ、スポーツ観戦</p>
         <p>スキル次第で場所に囚われない面と自由度の高さとかっこよさから
           この業界で働きたいと思い2023年後半から学習を始めました。勉強
           していく中でプログラミングの奥深さ、楽しさを知り完全にハマりました</p>
-        
       </div>
       
       
